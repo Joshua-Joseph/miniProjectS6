2acba6b21d06012ffcf23085fd6a340e2f5528de
Joshua Joseph <joshua841015@gmail.com>
2022-03-08 11:45:29 +0530

    link final update

--- link.py
+++ link.py
@@ -1,18 +1,21 @@
 
-# import modules
+# import module
 import requests
 import re
 
 from bs4 import BeautifulSoup
 
-# link to extract html data
+# link for extract html data
 def getdata(url):
 	r = requests.get(url)
 	return r.text
 
-htmldata = getdata("https://www.google.com/search?q=beautifulsoup+findall+links&biw=1301&bih=639&ei=LdwmYqTmO4iTseMPkcSb8AE&oq=findall+beautifulsoup+link+&gs_lcp=Cgdnd3Mtd2l6EAEYADIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeOgcIABBHELADOgQIABAKSgQIQRgASgQIRhgAUKMJWIITYOwhaAFwAXgAgAHbAYgBvgeSAQUwLjUuMZgBAKABAcgBCMABAQ&sclient=gws-wiz")
+htmldata = getdata("https://www.google.com/search?q=russsia&oq=russsia+&aqs=chrome..69i57.2172j0j9&sourceid=chrome&ie=UTF-8")
 soup = BeautifulSoup(htmldata, 'html.parser')
 
 for data in soup.find_all("a",href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
-	print((re.split(":(?=http)",data["href"].replace("/url?q=","")))[0])
+	print((re.split(":(?=http)",data["href"].replace("/url?q=","")))[0].split("&")[0])
 	
+
+
+
