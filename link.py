f2424c89818084ab46546eff22e36e7eaebcb63a
Joshua Joseph <joshua841015@gmail.com>
2022-03-08 11:05:26 +0530

    link done

--- link.py
+++ link.py
@@ -1,7 +1,11 @@
+
+# import modules
 import requests
+import re
 
 from bs4 import BeautifulSoup
 
+# link to extract html data
 def getdata(url):
 	r = requests.get(url)
 	return r.text
@@ -9,5 +13,6 @@ def getdata(url):
 htmldata = getdata("https://www.google.com/search?q=beautifulsoup+findall+links&biw=1301&bih=639&ei=LdwmYqTmO4iTseMPkcSb8AE&oq=findall+beautifulsoup+link+&gs_lcp=Cgdnd3Mtd2l6EAEYADIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeOgcIABBHELADOgQIABAKSgQIQRgASgQIRhgAUKMJWIITYOwhaAFwAXgAgAHbAYgBvgeSAQUwLjUuMZgBAKABAcgBCMABAQ&sclient=gws-wiz")
 soup = BeautifulSoup(htmldata, 'html.parser')
 
-for data in soup.find_all("a", href=True):
-	print(data['href'])
+for data in soup.find_all("a",href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
+	print((re.split(":(?=http)",data["href"].replace("/url?q=","")))[0])
+	
