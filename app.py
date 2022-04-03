f5496a22875b7897fc68b873253e94c76e731e72
gautham462 <gauthamkrishnaiit@gmail.com>
2022-04-03 12:31:42 +0530

    index api for testing deployment

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 CORS(app)
 
 
+@app.route("/", methods=["GET"])
+def index():
+    return jsonify(
+        {
+            "success": True,
+            "message": "We verify urls! But this isn't the place where you should be verifying...",
+        }
+    )
+
+
 @app.route("/verify", methods=["POST"])
 def verify():
     request_data = request.get_json(force=True)
