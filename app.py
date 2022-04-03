09d45d70a79d5791c618047f1543db0edaa68d91
gautham462 <gauthamkrishnaiit@gmail.com>
2022-04-03 12:13:46 +0530

    added flask api

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/verify", methods=["POST"])
+def verify():
+    request_data = request.get_json(force=True)
+    tab_url = request_data["tab_url"]
+    print(tab_url)
+    return jsonify({"success": True, "results": ["safe", "safe", "unsafe", "safe"]})
